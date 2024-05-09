9c0e31efad407dab217f25241054a9732a354447
Pronina <olia.prosha@gmail.com>
2024-05-09 10:26:43 +0300

    add tests

--- src/test/java/com/CrptApi/ApplicationTests.java
+++ src/test/java/com/CrptApi/ApplicationTests.java
@@ -1,9 +1,7 @@
 package com.CrptApi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
---
+++ src/test/java/com/CrptApi/CrptApiTest.java
@@ -0,0 +1,16 @@
+package com.CrptApi;
+
+import org.junit.jupiter.api.Test;
+
+
+class CrptApiTest {
+
+
+    @Test
+    void createDocument() {
+    }
+
+    @Test
+    void shutdown() {
+    }
+}
